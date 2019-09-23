c00f8fdba2362635250c7905ff2c75930ded06d9
bjliu5 <bjliu5@pc12w06.inf.elte.hu>
2019-09-23 09:26:20 +0200

    gyak3 - add Message

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,13 @@
-INSERT INTO FOO(id, name, created_at, updated_at) VALUES(1, 'Krisztián', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO FOO(id, name, created_at, updated_at) VALUES(2, 'Ádám', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO FOO(id, name, created_at, updated_at) VALUES(3, 'Márk', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO FOO(id, name, created_at, updated_at) VALUES(1, 'Krisztiï¿½n', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO FOO(id, name, created_at, updated_at) VALUES(2, 'ï¿½dï¿½m', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO FOO(id, name, created_at, updated_at) VALUES(3, 'Mï¿½rk', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 insert into issue (title, description, place, created_at, updated_at) values ('The issue #1', 'Fatallica', 'PC42', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
 insert into issue (title, description, place, created_at, updated_at) values ('MegaIssue', 'MegaFatal', 'PC12', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
 insert into issue (title, description, place, created_at, updated_at) values ('Unknown issue', 'U.F.O', 'Air', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
 insert into issue (title, description, place, created_at, updated_at) values ('Broken Windows 10', 'Everywhere', 'PCxxx', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+
+insert into message (text, created_at, updated_at) values ('The things', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+insert into message (text, created_at, updated_at) values ('that should', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+insert into message (text, created_at, updated_at) values ('not', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+insert into message (text, created_at, updated_at) values ('be', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
